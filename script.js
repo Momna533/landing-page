de18a5ffdbd99352368a51d922c07877a59d7716
Momna Ijaz <momna533gb@gmail.com>
2026-03-12 11:12:46 +0500

    adjusted images menu and widths in style.css file

--- script.js
+++ script.js
@@ -30,3 +30,26 @@ setInterval(() => {
   index = (index + 1) % testimonials.length;
   showTestimonial(index);
 }, 4000);
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const navItems = document.querySelectorAll(".nav-links li a");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+navItems.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
+
+document.addEventListener("click", (e) => {
+  const isClickInsideNav = navLinks.contains(e.target);
+  const isClickOnHamburger = hamburger.contains(e.target);
+
+  if (!isClickInsideNav && !isClickOnHamburger) {
+    navLinks.classList.remove("active");
+  }
+});
